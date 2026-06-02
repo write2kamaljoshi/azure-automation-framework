b4698f10f6dcab7094b353f8e059ee0bcd18656d
kamalkantjoshi <kamal.joshi@quovantis.com>
2026-06-02 12:58:52 +0530

    updated keyvault helper and azure-pipelines

--- azure_utils/keyvault_helper.py
+++ azure_utils/keyvault_helper.py
@@ -1,20 +1,38 @@
+# from azure.identity import DefaultAzureCredential
+# from azure.keyvault.secrets import SecretClient
+#
+#
+# KEY_VAULT_URL = "https://kv-sdet-learning-01.vault.azure.net/"
+#
+#
+# credential = DefaultAzureCredential()
+#
+# client = SecretClient(
+#     vault_url=KEY_VAULT_URL,
+#     credential=credential
+# )
+#
+#
+# def get_secret(secret_name):
+#
+#     retrieved_secret = client.get_secret(secret_name)
+#
+#     return retrieved_secret.value
+
+
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-
 
 KEY_VAULT_URL = "https://kv-sdet-learning-01.vault.azure.net/"
 
 
-credential = DefaultAzureCredential()
-
-client = SecretClient(
-    vault_url=KEY_VAULT_URL,
-    credential=credential
-)
-
-
 def get_secret(secret_name):
 
-    retrieved_secret = client.get_secret(secret_name)
+    credential = DefaultAzureCredential()
 
-    return retrieved_secret.value
+    client = SecretClient(
+        vault_url=KEY_VAULT_URL,
+        credential=credential
+    )
+
+    return client.get_secret(secret_name).value
